a00a1c530223a279a72e59074f39c460aed982ef
negpinkyy <negpinky@gmail.com>
2022-01-23 12:16:01 +0100

    first commit

---
+++ src/data.js
@@ -0,0 +1,94 @@
+const data = {
+    products: [
+        {
+            _id: '1',
+            name: 'Nike Slim Shirt',
+            category: 'Shirts',
+            image: '/image/clothe-1.jpg',
+            price: 120,
+            countInStock: 10,
+            brand: "Nike",
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high Quality Product'
+
+        },
+
+
+
+        {
+            _id: '2',
+            name: 'Adidas free Shirt',
+            category: 'Shirts',
+            image: '/image/clothe-2.jpg',
+            price: 120,
+            countInStock: 12,
+            brand: "Fila",
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high Quality Product'
+
+        },
+
+        {
+            _id: '3',
+            name: 'lacoste Slim Shirt',
+            category: 'Shirts',
+            image: '/image/clothe-3.jpg',
+            price: 100,
+            countInStock: 0,
+            brand: "Nike",
+            rating: 4,
+            numReviews: 10,
+            description: 'high Quality Product'
+
+        },
+
+        {
+            _id: '4',
+            name: 'Nike Slim Shirt',
+            category: 'Shoes',
+            image: '/image/shoes-1.jpg',
+            price: 120,
+            countInStock: 4,
+            brand: "Nike",
+            rating: 4.5,
+            numReviews: 14,
+            description: 'high Quality Product'
+
+        },
+
+        {
+            _id: '5',
+            name: 'Puma',
+            category: 'Shoes',
+            image: '/image/shoes-2.jpg',
+            price: 120,
+            countInStock: 15,
+            brand: "Nike",
+            rating: 4.5,
+            numReviews: 20,
+            description: 'high Quality Product'
+
+        },
+
+        {
+
+            _id: '6',
+            name: 'Nike Slim Shirt',
+            category: 'Sh0es',
+            image: '/image/shoes-3.jpg',
+            price: 120,
+            countInStock: 10,
+            brand: "Lacoste",
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high Quality Product'
+
+
+        },
+    ]
+};
+
+
+export default data;
